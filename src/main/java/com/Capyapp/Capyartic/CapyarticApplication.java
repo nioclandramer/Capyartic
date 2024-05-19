52bdffcb2f28151d898e5ac3b7b65bf235bba363
nioclandramer <161773056+nioclandramer@users.noreply.github.com>
2024-05-19 11:06:25 -0500

    all

---
+++ src/main/java/com/Capyapp/Capyartic/CapyarticApplication.java
@@ -0,0 +1,13 @@
+package com.Capyapp.Capyartic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapyarticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapyarticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Capyapp/Capyartic/CapyarticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Capyapp.Capyartic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapyarticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
