102cb8c0884f1238629c460759204e0c7990aec1
nioclandramer <161773056+nioclandramer@users.noreply.github.com>
2024-05-19 11:41:31 -0500

    Entitys

--- src/main/java/com/Capyapp/Capyartic/Entidades/Usuario.java
+++ src/main/java/com/Capyapp/Capyartic/Entidades/Usuario.java
@@ -1,4 +1,31 @@
 package com.Capyapp.Capyartic.Entidades;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String primerNombre;
+    private String segundoNombre;
+    @Column(nullable = false)
+    private String primerApellido;
+    private String segundoApellido;
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    private String direccion;
+
 }
