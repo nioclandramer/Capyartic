802a8fd26dd430f483191b91be06424be4041d13
GuilllermoPP <142278424+GuilllermoPP@users.noreply.github.com>
2024-05-19 14:46:38 -0500

    Entidad ca..b

--- src/main/java/com/Capyapp/Capyartic/Entidades/Tutor.java
+++ src/main/java/com/Capyapp/Capyartic/Entidades/Tutor.java
@@ -7,8 +7,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
-@Table
+@Table(name = "Tutors")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,6 +20,19 @@ public class Tutor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne(optional = false)
+    @JoinColumn(name = "usuarioId",referencedColumnName = "id")
+    private Usuario usuario;
 
+    @ManyToMany
+    @JoinTable(
+            name = "tutorCategoria",
+            joinColumns = @JoinColumn(name = "tutorId",referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn( name = "categoriaId",referencedColumnName = "id")
+    )
+    private List<Categoria> categorias = new ArrayList<>();
 
+    public void addCategoria(Categoria categoria) {
+        this.categorias.add(categoria);
+    }
 }
