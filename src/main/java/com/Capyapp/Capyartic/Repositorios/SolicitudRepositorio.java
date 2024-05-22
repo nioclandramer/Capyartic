ba31371d8a2d76cd16103e03429970f09d4f82f0
blackbox002 <mauricioecheverri007@gmail.com>
2024-05-22 14:43:11 -0500

    cambio rol a enum y eliminar su repestiva tabla

---
+++ src/main/java/com/Capyapp/Capyartic/Repositorios/SolicitudRepositorio.java
@@ -0,0 +1,10 @@
+package com.Capyapp.Capyartic.Repositorios;
+
+import com.Capyapp.Capyartic.Entidades.Solicitud;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SolicitudRepositorio extends JpaRepository<Solicitud,Long> {
+    List<Solicitud> findSolicitudByTutor(Long idTutor);
+}
--- src/main/java/com/Capyapp/Capyartic/Repositorios/temaRepositorio.java
+++ src/main/java/com/Capyapp/Capyartic/Repositorios/temaRepositorio.java
@@ -4,4 +4,5 @@ import com.Capyapp.Capyartic.Entidades.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface temaRepositorio extends JpaRepository<Tema,Long> {
+    Tema findTemaByNombre(String nombre);
 }
