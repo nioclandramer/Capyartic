71b489d890e56711b83f918e09df87c9ad176d70
nioclandramer <161773056+nioclandramer@users.noreply.github.com>
2024-05-26 14:43:42 -0500

    Dto

--- src/main/java/com/Capyapp/Capyartic/Dto/Usuario/UsuarioToSaveDto.java
+++ src/main/java/com/Capyapp/Capyartic/Dto/Usuario/UsuarioToSaveDto.java
@@ -5,7 +5,7 @@ import com.Capyapp.Capyartic.Entidades.Rol;
 
 import java.time.LocalDateTime;
 
-public record UsuarioToSaveDto(Long id, String primerNombre, String segundoNombre, String primerApellido, String segundoApellido,
+public record UsuarioToSaveDto(String primerNombre, String segundoNombre, String primerApellido, String segundoApellido,
                                String email, Integer numeroTelefono, String sexo, String contraseña, String nombreUsuario,
                                LocalDateTime fechaRegistro, LocalDateTime fechaNacimiento, Rol rolUser, EstadoUsuario estadoUsuario) {
 }
