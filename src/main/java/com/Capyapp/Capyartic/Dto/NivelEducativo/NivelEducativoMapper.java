4f5a959164740ccd9885a52e49982df478165d03
nioclandramer <161773056+nioclandramer@users.noreply.github.com>
2024-05-29 16:44:00 -0500

    Mapper

--- src/main/java/com/Capyapp/Capyartic/Dto/NivelEducativo/NivelEducativoMapper.java
+++ src/main/java/com/Capyapp/Capyartic/Dto/NivelEducativo/NivelEducativoMapper.java
@@ -1,10 +1,16 @@
 package com.Capyapp.Capyartic.Dto.NivelEducativo;
 
+import com.Capyapp.Capyartic.Entidades.NivelEducativo;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 
+@Component
+@Mapper(componentModel = "spring")
 public interface NivelEducativoMapper {
     NivelEducativoMapper INSTANCE = Mappers.getMapper(NivelEducativoMapper.class);
+
+    NivelEducativo toNivelEducativoDto(NivelEducativoDto nivelEducativoDto);
 
     NivelEducativoDto toNivelEducativoDto(NivelEducativoToSaveDto nivelEducativoToSaveDto);
 
