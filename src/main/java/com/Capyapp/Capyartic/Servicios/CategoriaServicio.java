cab4d5f93ff928f5623e37f558bf2b2173065cff
blackbox002 <mauricioecheverri007@gmail.com>
2024-06-01 16:15:40 -0500

    cambio rol a enum y eliminar su repestiva tabla

--- src/main/java/com/Capyapp/Capyartic/Servicios/CategoriaServicio.java
+++ src/main/java/com/Capyapp/Capyartic/Servicios/CategoriaServicio.java
@@ -1,4 +1,14 @@
 package com.Capyapp.Capyartic.Servicios;
 
+import com.Capyapp.Capyartic.Dto.Categoria.CategoriaDto;
+import com.Capyapp.Capyartic.Dto.Categoria.CategoriaToSaveDto;
+import com.Capyapp.Capyartic.Excepciónes.CategoriaNotFoundException;
+import org.springframework.stereotype.Service;
+
+
 public interface CategoriaServicio {
+    CategoriaDto guardarCategoria(CategoriaToSaveDto categoria);
+    CategoriaDto actualizarCategoria(Long id,CategoriaToSaveDto categoria);
+    CategoriaDto findById(Long id)throws CategoriaNotFoundException;
+    void deleteById(Long id)throws CategoriaNotFoundException;
 }
--- src/main/java/com/Capyapp/Capyartic/Servicios/CategoriaServicioIMPL.java
+++ src/main/java/com/Capyapp/Capyartic/Servicios/CategoriaServicioIMPL.java
@@ -1,4 +1,46 @@
 package com.Capyapp.Capyartic.Servicios;
 
-public class CategoriaServicioIMPL {
+import com.Capyapp.Capyartic.Dto.Categoria.CategoriaDto;
+import com.Capyapp.Capyartic.Dto.Categoria.CategoriaMapper;
+import com.Capyapp.Capyartic.Dto.Categoria.CategoriaToSaveDto;
+import com.Capyapp.Capyartic.Entidades.Categoria;
+import com.Capyapp.Capyartic.Excepciónes.CategoriaNotFoundException;
+import com.Capyapp.Capyartic.Repositorios.CategoriaRepositorio;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaServicioIMPL implements  CategoriaServicio  {
+    private final CategoriaRepositorio categoriaRepositorio;
+
+    public CategoriaServicioIMPL(CategoriaRepositorio categoriaRepositorio) {
+        this.categoriaRepositorio = categoriaRepositorio;
+    }
+
+    @Override
+    public CategoriaDto guardarCategoria(CategoriaToSaveDto categoria) {
+        Categoria categoria1= CategoriaMapper.INSTANCE.CategoriaToSaveDtoToCategoria(categoria);
+        Categoria categoriaGuardada= categoriaRepositorio.save(categoria1);
+        return CategoriaMapper.INSTANCE.categoriaToCategoriaDto(categoriaGuardada);
+    }
+
+    @Override
+    public CategoriaDto actualizarCategoria(Long id, CategoriaToSaveDto categoria) {
+        Categoria categoria1=CategoriaMapper.INSTANCE.CategoriaToSaveDtoToCategoria(categoria);
+        Categoria categoriaExiste=categoriaRepositorio.findById(id).orElseThrow(()-> new CategoriaNotFoundException("Categoría no existe"));
+        categoriaExiste.setDescripcion(categoria1.getDescripcion());
+        categoriaExiste.setNombre(categoria1.getNombre());
+        return CategoriaMapper.INSTANCE.categoriaToCategoriaDto(categoriaExiste);
+    }
+
+    @Override
+    public CategoriaDto findById(Long id) throws CategoriaNotFoundException {
+        Categoria categoria= categoriaRepositorio.findById(id).orElseThrow(()-> new CategoriaNotFoundException("Categoria no existe"));
+        return CategoriaMapper.INSTANCE.categoriaToCategoriaDto(categoria);
+    }
+
+    @Override
+    public void deleteById(Long id) throws CategoriaNotFoundException {
+        Categoria categoria= categoriaRepositorio.findById(id).orElseThrow(()-> new CategoriaNotFoundException("Categoria no existe"));
+        categoriaRepositorio.deleteById(id);
+    }
 }
