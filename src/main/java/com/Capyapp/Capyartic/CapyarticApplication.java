0deb1d360ef21be5f63c0c2a60cd441c4e3e2011
blackbox002 <mauricioecheverri007@gmail.com>
2024-06-10 13:14:01 -0500

    cambio rol a enum y eliminar su repestiva tabla

--- src/main/java/com/Capyapp/Capyartic/CapyarticApplication.java
+++ src/main/java/com/Capyapp/Capyartic/CapyarticApplication.java
@@ -2,6 +2,9 @@ package com.Capyapp.Capyartic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CapyarticApplication {
@@ -10,4 +13,14 @@ public class CapyarticApplication {
 		SpringApplication.run(CapyarticApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+
+	}
 }
